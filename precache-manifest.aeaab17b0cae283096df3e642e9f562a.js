616abfd9829e3f81de419d118d75afa4bbdef2a2
ivnblv <ivnb.web@gmail.com>
2019-06-01 01:26:18 +0300

    Updates

--- precache-manifest.aeaab17b0cae283096df3e642e9f562a.js
+++ precache-manifest.aeaab17b0cae283096df3e642e9f562a.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ef889b0888a8e50d03c2f633f1d6bbe4",
+    "revision": "b20b01af9726cf14aee5a7cc0ba4822d",
     "url": "/tvshowinfo/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/tvshowinfo/static/css/2.5d832d7a.chunk.css"
   },
   {
-    "revision": "1dac23f167e6fc295e4a",
+    "revision": "f0b6c68a5d59929df904",
     "url": "/tvshowinfo/static/css/main.22f2efb2.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/tvshowinfo/static/js/2.9b40753c.chunk.js"
   },
   {
-    "revision": "1dac23f167e6fc295e4a",
-    "url": "/tvshowinfo/static/js/main.29dfd6ec.chunk.js"
+    "revision": "f0b6c68a5d59929df904",
+    "url": "/tvshowinfo/static/js/main.bfd745cf.chunk.js"
   },
   {
     "revision": "ebc2b77c4f320889e3e2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tvshowinfo/precache-manifest.e5064d4279a6f81b834e73da5b0a68e6.js"
+  "/tvshowinfo/precache-manifest.aeaab17b0cae283096df3e642e9f562a.js"
 );
 
 self.addEventListener('message', (event) => {
