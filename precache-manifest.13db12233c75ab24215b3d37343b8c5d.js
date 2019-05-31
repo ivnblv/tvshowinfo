cdde8e9b48f23d64f00b68923d0ae38e1a83ea8d
ivnblv <ivnb.web@gmail.com>
2019-05-31 20:35:54 +0300

    Updates

---
+++ precache-manifest.13db12233c75ab24215b3d37343b8c5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1442920d6fb18f46137c56c886d6d610",
+    "url": "/tvshowinfo/index.html"
+  },
+  {
+    "revision": "2e95b3b94129faf0e435",
+    "url": "/tvshowinfo/static/css/2.5d832d7a.chunk.css"
+  },
+  {
+    "revision": "44785246f31a5260bac8",
+    "url": "/tvshowinfo/static/css/main.a973d3d4.chunk.css"
+  },
+  {
+    "revision": "2e95b3b94129faf0e435",
+    "url": "/tvshowinfo/static/js/2.9b40753c.chunk.js"
+  },
+  {
+    "revision": "44785246f31a5260bac8",
+    "url": "/tvshowinfo/static/js/main.94cfef84.chunk.js"
+  },
+  {
+    "revision": "ebc2b77c4f320889e3e2",
+    "url": "/tvshowinfo/static/js/runtime~main.fbbd1e26.js"
+  }
+]);
